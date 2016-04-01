4f79d1644d15110185d6dc0f54bfb7a9d55f70fd
Chris Enríquez <christoferen7@gmail.com>
2016-04-01 07:55:23 -0600

    Configuración del servidor, aún quedan varias cosas pendientes por hacer

--- public/js/app.js
+++ public/js/app.js
@@ -4,38 +4,3 @@
 */
 
 'use strict';
-
-function handleOrientation (event) {
-    var absolute = event.absolute || 0;
-    var alpha    = event.alpha || 0;
-    var beta     = event.beta || 0;
-    var gamma    = event.gamma || 0;
-
-    var alphaSpan   = document.querySelector('#alpha-position');
-    var betaSpan    = document.querySelector('#beta-position');
-    var gammaSpan   = document.querySelector('#gamma-position');
-
-    alphaSpan.innerHTML = alpha;
-    betaSpan.innerHTML  = beta;
-    gammaSpan.innerHTML = gamma;
-
-    document.querySelector('#temp').innerHTML = absolute;
-}
-
-function containersSize () {
-    document.querySelector('#image-container').style.height     = window.innerHeight + 'px';
-    document.querySelector('#options-container').style.height   = window.innerHeight + 'px';
-}
-
-//  Obteniendo el valor del alto de la pantalla
-document.addEventListener('DOMContentLoaded', function (e) {
-    //  Tamaño del fondo
-    containersSize();
-
-    //  Manipulación de la orientación del dispositivo
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", handleOrientation);
-    } else {
-        console.log("Sorry, your browser doesn't support Device Orientation");
-    }
-});
--- server.js
+++ server.js
@@ -5,7 +5,11 @@
 'use strict';
 
 var express = require('express');
+var session = require('express-session');
 var app     = express();
+var http    = require('http').Server(app);
+var service = require('./services/services');
+var io      = require('socket.io')(http);
 
 /********************************
 Block the header from containing information about the server
@@ -28,33 +32,55 @@ Create a directory called public and then a directory named img inside of it and
 ********************************/
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'rolling-spider'
+}));
 
 /********************************
 ROUTES
 ********************************/
 app.get('/', function (req, res) {
+    let connect = service.connect(req.session);
+    //console.log(connect);
     res.render('home');
+    /********************************
+    Creating socket
+    ********************************/
+    var nsp = io.of('/rolling-chanel');
+    nsp.on('connection', function (socket) {
+      console.log('someone connected')
+    });
+    nsp.emit('hi', 'everyone!');
+
+    io.on('connection', function(socket){
+        socket.join('some room');
+    });
+    // io.on('connection', function (socket) {
+    //     socket.emit('news', { hello: 'world' });
+    //     socket.on('my other event', function (data) {
+    //         console.log(data);
+    //     });
+    // });
 });
 app.get('/rollingadmin', function (req, res) {
+    //console.log(io.of('/rolling-chanel').clients());
+
+    let clients = io.sockets.adapter.rooms['some room'];
+    console.log(clients);//works
     res.render('admin');
 });
-
-var http = require('http').Server(app);
-var io      = require('socket.io')(http);
-
-/********************************
-Creating socket
-********************************/
-io.on('connection', function (socket) {
-  console.log('a user connected');
-});
-
 
 /********************************
 Creating the server
 ********************************/
 http.listen(3001, function () {
-    console.log('listening on *:3000');
+    let host = 'localhost';
+    let port = this.address().port;
+
+    console.log('Servidor en ruta http://%s:%s', host, port);
+    //console.log('listening on *:3000');
 });
 // var server = app.listen(3001, function () {
 //     var host = 'localhost';
